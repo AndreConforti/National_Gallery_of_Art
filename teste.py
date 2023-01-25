20529eb9030cd42c72e9c377af5752cc9ec2a508
AndreConforti <andre.r.m.conforti@gmail.com>
2023-01-25 00:02:38 -0300

    Atualizada a paginação. Agora é preciso verificar os erros no momento da coleta das informações

--- teste.py
+++ teste.py
@@ -2,121 +2,71 @@
 import requests
 from bs4 import BeautifulSoup as bs
 import csv
+from time import sleep
+import pandas as pd
 
 
 # Criar um arquivo CSV para gravar, adicionar linha de cabeçalhos
-f = csv.writer(open('a-z-artist-names.csv', 'w'))
-f.writerow(['Name', 'Link', 'Nationality', 'Birth', 'Demise'])
 
 alfabeto = 'A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z'.split(',')
 pages = []
 numbers = []
+artists = []
 
 for letra in alfabeto:
-    pagenumber = 1
-    pagenumbers = 1
-
-    while pagenumber <= pagenumbers:
-        try:
-            url = "https://web.archive.org/web/20121007172955/http://www.nga.gov/collection/an" + letra + str(pagenumber) + '.htm'
-
-            # Coletar a página da lista de artistas
-            page = requests.get(url)
-            # Criar o objeto BeaitufulSoup
+    for number in range(1, 100):
+        url = "https://web.archive.org/web/20121007172955/http://www.nga.gov/collection/an" + letra + str(number) + '.htm'
+        sleep(5)
+        ## Coletar a página da lista de artistas
+        page = requests.get(url)
+        if page.status_code == 200:          
+            print(f'Coletando as informações da página {letra} - {number}')
+            ## Criar o objeto BeaitufulSoup
             soup = bs(page.text, 'html.parser')
+            ## Pegar todo o texto da div AlphaNav e excluir
+            last_links = soup.find(class_='AlphaNav') 
+            last_links.decompose()
+            ## Pegar todo o texto da div BodyText
+            lista_artistas = soup.find(class_='BodyText')
 
-            # Pegar todo o texto da div AlphaNav
-            last_links = soup.find(class_='AlphaNav')
-            # Pegar o texto de todas as instâncias da tag <a> dentro da div AlphaNav
-            page_links = last_links.find_all('a')
-            
-            pagenumbers = len(page_links) + 1       
+            dados_artistas = lista_artistas.find_all('tr')
 
-        except:
-            print("Não existe a página requisitada")
-            break
+            for linha in dados_artistas:
+                completo = linha.find_all('td')
+                nome = completo[0]
+                nome = nome.find('a')
+                artist_name = nome.contents[0]
+                link = 'https://web.archive.org' + nome.get('href')
+                restante = completo[1].text.split(',')
+                nationality = restante[0]
+                try:
+                    periodo = restante[1].split('-')
+                    birth = periodo[0].strip()
+                    demise = periodo[1].strip()
+                except:
+                    try:
+                        birth = restante[1].strip()
+                        demise = 'Unknown'
+                    except:
+                        nationality = 'Unknown'
+                        birth = 'Unknown'
+                        demise = 'Unknown'
+                ## Adicionar em uma linha o nome de cada artista e o link associado
+                artist = {
+                    'artist_name' : artist_name,
+                    'link' : link,
+                    'nationality' : nationality,
+                    'birth' : birth,
+                    'demise' : demise
+                }
+                artists.append(artist.copy())
         else:
-            # print(f'Coletando as informações da página {letra} - {pagenumber}')
-            # last_links.decompose()
-            # # Pegar todo o texto da div BodyText
-            # lista_artistas = soup.find(class_='BodyText')
-
-            # dados_artistas = lista_artistas.find_all('tr')
-
-            # for linha in dados_artistas:
-            #     completo = linha.find_all('td')
-            #     nome = completo[0]
-            #     nome = nome.find('a')
-            #     artist_name = nome.contents[0]
-            #     link = 'https://web.archive.org' + nome.get('href')
-            #     restante = completo[1].text.split(',')
-            #     nationality = restante[0]
-            #     try:
-            #         periodo = restante[1].split('-')
-            #         birth = periodo[0].strip()
-            #         demise = periodo[1].strip()
-            #     except:
-            #         try:
-            #             birth = restante[1].strip()
-            #             demise = 'Unknown'
-            #         except:
-            #             nationality = 'Unknown'
-            #             birth = 'Unknown'
-            #             demise = 'Unknown'
-            #     # Adicionar em uma linha o nome de cada artista e o link associado
-            #     f.writerow([artist_name, link, nationality, birth, demise])
+            print("Página não encontrada")
+            break
+    break
                         
-            pagenumber += 1
-            print(pagenumber)
-            print(pagenumbers)
-  
+df = pd.DataFrame(artists)
 
-
-# url = "https://web.archive.org/web/20121007172955/http://www.nga.gov/collection/anA2.htm"
-# print(url)
-
-# # Coletar a página da lista de artistas
-# page = requests.get(url)
-# # Criar o objeto BeaitufulSoup
-# soup = bs(page.text, 'html.parser')
-# # Pegar todo o texto da div AlphaNav
-# last_links = soup.find(class_='AlphaNav')
-# last_links.decompose()
-# # Pegar todo o texto da div BodyText
-# lista_artistas = soup.find(class_='BodyText')
-
-# dados_artistas = lista_artistas.find_all('tr')
-
-# for linha in dados_artistas:
-#     completo = linha.find_all('td')
-#     nome = completo[0]
-#     nome = nome.find('a')
-#     nome_artista = nome.contents[0]
-#     link = 'https://web.archive.org' + nome.get('href')
-#     restante = completo[1].text.split(',')
-#     nacionalidade = restante[0]
-#     try:
-#         periodo = restante[1].split('-')
-#         nascimento = periodo[0].strip()
-#         falescimento = periodo[1].strip()
-#     except:
-#         try:
-#             nascimento = restante[1].strip()
-#             falescimento = 'Unknown'
-#         except:
-#             nacionalidade = 'Unknown'
-#             nascimento = 'Unknown'
-#             falescimento = 'Unknown'
-#     # Adicionar em uma linha o nome de cada artista e o link associado
-#     f.writerow([nome_artista, link, nacionalidade, nascimento, falescimento])
-
-
-
-
-
-
-
-
-
+df.to_csv('artistas_a_z.csv', sep=(';'))
 
 
